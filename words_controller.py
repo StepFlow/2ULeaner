43b60a020b7575b60d94be73a2267d79a02b083a
Stephen <flowstepg@gmail.com>
2017-04-22 14:15:54 +1000

    first commit

---
+++ words_controller.py
@@ -0,0 +1,53 @@
+from words_model import TextParser
+from pynput.keyboard import Key, Listener
+import queue
+import time
+
+
+q = queue.Queue()
+
+
+def on_press(key):
+    global q
+
+    if key == Key.right:
+        q.put(True)
+
+    if key == Key.left:
+        q.put(False)
+
+listener = Listener(on_press=on_press)
+
+
+class Controller:
+    global listener
+
+    def __init__(self):
+        listener.start()
+
+    def __del__(self):
+        listener.stop()
+
+    def is_known(self):
+        while q.empty():
+            time.sleep(0.1)
+
+        return q.get()
+
+
+def main(filename):
+    try:
+        with open(filename) as fin:
+            text = fin.read()
+
+        text_parser = TextParser(text)
+        controller = Controller()
+
+        for word in text_parser:
+            print(word)
+            text_parser.add_last_word_to_db(controller.is_known())
+
+    except FileNotFoundError:
+        print('File "' + filename + '" was not found')
+
+main("input.txt")
---
+++ words_model.py
@@ -0,0 +1,48 @@
+import re
+import sqlite3
+
+
+class TextParser:
+    def __init__(self, text):
+        self.word_pattern = \
+            re.compile(r'(^|[\s])?[\'()]?([A-Za-z]+-?[A-Za-z]+)')
+        self.text = text
+        self.word_iterator = re.finditer(self.word_pattern, self.text)
+        self.last_word = 'hi'
+        self.db_connection = sqlite3.connect('words_db.sqlite')
+        self.db_cursor = self.db_connection.cursor()
+        self.select_pattern = \
+            "SELECT id FROM words WHERE word = :word"
+        self.insert_pattern = \
+            "INSERT INTO words (word, learned) VALUES (:word, :learned)"
+
+    def __del__(self):
+        self.db_connection.commit()
+        self.db_connection.close()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        word = next(self.word_iterator).group(2).lower()
+
+        while self.word_is_in_db(word):
+            word = next(self.word_iterator).group(2).lower()
+
+        self.last_word = word
+
+        return word
+
+    def word_is_in_db(self, word):
+        self.db_cursor.execute(self.select_pattern, {"word": word})
+
+        if self.db_cursor.fetchone() is None:
+            return False
+
+        return True
+
+    def add_last_word_to_db(self, known):
+        self.db_cursor.execute(
+            self.insert_pattern,
+            {"word": self.last_word, "learned": known}
+        )
